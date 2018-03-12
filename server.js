af09730993ace0f14faced369513d2b4ce7b713d
Mjyunichi <32351449+Mjyunichi@users.noreply.github.com>
2018-03-12 15:57:48 +0900

    Update server.js

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ app.get('/', function (req, res) {
   }
 });
 
-app.get('/*', function (req, res) {
+app.get('/aaa', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
   if (!db) {
