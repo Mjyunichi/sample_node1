bcf2c027e5c77b7f4f239705beab3b6a77bdcc5d
Mjyunichi <32351449+Mjyunichi@users.noreply.github.com>
2018-03-12 16:15:49 +0900

    Update server.js

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ app.get('/', function (req, res) {
   }
 });
 
-app.get('/*', function (req, res) {
+app.get('/pagecount', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
   if (!db) {
